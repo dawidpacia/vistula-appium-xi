3282c0ced5c510aa8d638fad0e66adaf727471fe
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-03-20 15:24:12 +0100

    tests calc

--- sample_test.py
+++ sample_test.py
@@ -1,6 +1,15 @@
-def test_false():
-    assert True == False
+from reviesion.task_calculator import Calculator
 
+calc = Calculator()
 
-def test_true():
-    assert True == True
+def test_add():
+    assert calc.sum(1, 2) == 3
+
+def test_division():
+    assert calc.divide(1, 2) == 0.5
+
+def test_division_by_zero():
+    assert calc.divide(1, 0) == False
+
+def test_multiply():
+    assert calc.multiply(-1, -1) == 1
