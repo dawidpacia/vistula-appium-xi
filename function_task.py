422e7e82f6f0e61eb20a99ca80ecf01c0d675771
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-03-20 12:34:14 +0100

    tasks

---
+++ function_task.py
@@ -0,0 +1,6 @@
+def add_values(a, b):
+    result = a + b
+    return result
+
+print(add_values(1, 2))
+
---
+++ loop_task.py
@@ -0,0 +1,14 @@
+for i in range(10, 100, 2):
+    print(i)
+
+
+for i in range(100):
+    if i % 2 == 0 and i >= 10:
+        print(i)
+
+
+a, b = 10, 99
+
+while a < b:
+    print(a)
+    a += 2
