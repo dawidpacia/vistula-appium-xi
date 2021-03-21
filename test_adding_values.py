ec39b3c3ed657fc4a9c9e60b51d559f1af2b39fa
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-03-21 16:17:25 +0100

    testing calc

--- test_adding_values.py
+++ test_adding_values.py
@@ -1,39 +1,36 @@
-
-
 import os
 from appium import webdriver
 
-# Returns abs path relative to this file and not cwd
-# app_name = "calculator.apk"
-# app_path = os.path.abspath(os.path.join(os.path.dirname(__file__), app_name))
-#
-#
-# desired_caps = {}
-# desired_caps['platformName'] = 'Android'
-# desired_caps['newCommandTimeout'] = 250
-# desired_caps['app'] = app_path
-#
-# driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+value_1 = 1
+value_2 = 8
 
 
-value_1 = 2
-value_2 = 3
+class TestCalculator:
 
+    def test_add_value(self, driver):
+        driver.find_element_by_id(f"digit_{value_1}").click()
+        driver.find_element_by_id("op_add").click()
+        driver.find_element_by_id(f"digit_{value_2}").click()
+        driver.find_element_by_accessibility_id("equals").click()
+        result = driver.find_element_by_id("result_final").text
 
-def test_add_value(driver):
-    driver.find_element_by_id(f"digit_{value_1}").click()
-    driver.find_element_by_id("op_add").click()
-    driver.find_element_by_id(f"digit_{value_2}").click()
-    driver.find_element_by_accessibility_id("equals").click()
-    result = driver.find_element_by_id("result_final").text
+        assert value_1 + value_2 == int(result)
 
-    assert value_1 + value_2 == int(result)
+    def test_divide_value(self, driver):
+        driver.find_element_by_id(f"digit_{value_1}").click()
+        driver.find_element_by_id("op_div").click()
+        driver.find_element_by_id(f"digit_{value_2}").click()
+        driver.find_element_by_accessibility_id("equals").click()
+        result = driver.find_element_by_id("result_final").text
 
-def test_add_value_2(driver):
-    driver.find_element_by_id(f"digit_{value_1}").click()
-    driver.find_element_by_id("op_add").click()
-    driver.find_element_by_id(f"digit_{value_2}").click()
-    driver.find_element_by_accessibility_id("equals").click()
-    result = driver.find_element_by_id("result_final").text
+        assert value_1 / value_2 == float(result)
 
-    assert value_1 + value_2 == int(result)
+    def test_sub_value(self, driver):
+        driver.find_element_by_id(f"digit_{value_1}").click()
+        driver.find_element_by_id("op_div").click()
+        driver.find_element_by_id(f"digit_{value_2}").click()
+        driver.find_element_by_accessibility_id("equals").click()
+        result = driver.find_element_by_id("result_final").text
+
+        assert value_1 / value_2 == float(result)
