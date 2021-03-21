f9f449d97351b88fdefd0d84ebeb138687bfdb16
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-03-21 11:18:42 +0100

    appium 1st commit

---
+++ reviesion/fuel_tests.py
@@ -0,0 +1,26 @@
+from reviesion.function_task import calculate_fuel
+
+
+def test_12():
+    assert calculate_fuel(12) == 2
+
+def test_string():
+    assert calculate_fuel("test") == False
+
+def test_float():
+    assert calculate_fuel(12.0) == 2
+
+def test_negative_value():
+    assert calculate_fuel(-2) == False
+
+def test_zero():
+    assert calculate_fuel(0) == False
+
+def test_1():
+    assert calculate_fuel(1) == 1
+
+def test_8():
+    assert calculate_fuel(8) == 1
+
+def test_9():
+    assert calculate_fuel(9) == 1
--- reviesion/function_task.py
+++ reviesion/function_task.py
@@ -2,8 +2,14 @@ from math import floor
 
 
 def calculate_fuel(mass):
-    fuel = mass // 3 - 2
-    return fuel
+    if type(mass) in [int, float] and mass > 0:
+        if mass < 9:
+            return 1
+        else:
+            fuel = mass // 3 - 2
+            return fuel
+    else:
+        return False
 
 
 def calculate_fuel_math_floor(mass):
